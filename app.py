915620b1a6c2541f9ce4910d88fa045a70a23b8b
JaoCord <jgboybryt@gmail.com>
2024-11-07 14:33:18 -0300

    Atualizei o código e quase completei o front end

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('inicio.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/comeco')
+def comeco():
+    return render_template('comeco.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
